49e0c53f06d9208140440f703594fe249ad39995
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-03-24 12:03:23 +0100

    added files from initial session

---
+++ WPFAdam/InputListener.cs
@@ -0,0 +1,78 @@
+﻿using Advantech.Adam;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAdam
+{
+    public class InputListener
+    {
+        private BackgroundWorker wrkr = new BackgroundWorker();
+        public int startpoint { get; set; } = 2;
+        public int amount { get; set; } = 6;
+        public Modbus modbus { get; set; }
+
+        public event btnPressedEventHandler btnPressed;
+        public event redSwitchedEventHandler redSwitched;
+        
+        public InputListener(Modbus mod)
+        {
+            this.modbus = mod;
+            wrkr.DoWork += Wrkr_DoWork;
+            wrkr.RunWorkerCompleted += Wrkr_RunWorkerCompleted;
+
+            wrkr.RunWorkerAsync();
+        }
+
+        private void Wrkr_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            int[] res = (int[]) e.Result;
+
+            if(res[0] == 1 && res[1] == 0 && res[2] == 1)
+            {
+                btnPressed(res[0]);
+            }
+            if(res[0] == 2 && res[1] == 0 && res[2] == 1)
+            {
+                btnPressed(res[0]);
+            }
+            if(res[0] == 0)
+            {
+                redSwitched(res[2] == 1);
+            }
+            wrkr.RunWorkerAsync();
+        }
+
+        private void Wrkr_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bool[] prevs = new bool[amount];
+            while (!modbus.ReadCoilStatus(startpoint, amount, out prevs)) { }
+            bool[] curs = new bool[amount];
+            bool looping = true;
+            while(looping)
+            {
+                while (!modbus.ReadCoilStatus(startpoint, amount, out curs)) { }
+
+                for(int i = 0; i< amount; i++)
+                {
+                    if(prevs[i] != curs[i])
+                    {
+                        int p;
+                        int c;
+                        if (prevs[i]) p = 1; else p = 0;
+                        if (curs[i]) c = 1; else c = 0;
+                        e.Result = new int[3] {i,p, c };
+                        looping = false;
+                    }
+                }
+
+            }
+        }
+
+        public delegate void btnPressedEventHandler(int nr);
+        public delegate void redSwitchedEventHandler(bool isOn);
+    }
+}
---
+++ WPFAdam/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Advantech.Adam;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAdam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AdamSocket Socket { get; set; }
+        public Modbus modbus { get; set; }
+        public bool bl { get; set; } = true;
+        public InputListener inputs { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Socket = new AdamSocket();
+            Socket.Connect("192.168.1.2", ProtocolType.Tcp, 502);
+            modbus = new Modbus(Socket);
+            inputs = new InputListener(modbus);
+            inputs.btnPressed += Inputs_btnPressed;
+            inputs.redSwitched += Inputs_redSwitched;
+        }
+
+        private void Inputs_redSwitched(bool isOn)
+        {
+            if(isOn)
+            {
+                Console.WriteLine("ON");
+
+            }
+            else Console.WriteLine("OFF");
+
+        }
+
+        private void Inputs_btnPressed(int nr)
+        {
+            if(nr == 1)
+            {
+                Console.WriteLine("Green!!");
+            }
+            if(nr == 2)
+            {
+                Console.WriteLine("Black!!");
+            }
+        }
+
+       
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            bool[] inputs = new bool[4];
+            bool b = modbus.ReadCoilStatus(1, 4, out inputs );
+
+            Console.WriteLine(inputs[2]);
+        }
+    }
+}
