e311e94a71f9b3983af89ad8743b70740b8d49bc
Bruno Haspeslagh <Bruno Haspeslagh>
2017-03-31 10:34:33 +0200

    integrated UI and adam logic

--- WPFAdam/MainWindow.xaml.cs
+++ WPFAdam/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPFAdam
 {
@@ -25,6 +26,12 @@ namespace WPFAdam
     public partial class MainWindow : Window
     {
         public AdamSocket Socket { get; set; }
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        DispatcherTimer rotateVentilatorTimer = new DispatcherTimer();
+        int ventilatorDraaistatus;
+        Color colorLightOn = Color.FromRgb(255, 0, 0);
+        Color colorLightOff = Color.FromRgb(0, 0, 0);
+        bool[] statusled = new bool[5];
         public Modbus modbus { get; set; }
         public bool bl { get; set; } = true;
         public bool fan { get; set; } = false;
@@ -33,6 +40,10 @@ namespace WPFAdam
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            rotateVentilatorTimer.Tick += new EventHandler(rotateVentilatorTimer_Tick);
+            rotateVentilatorTimer.Interval = new TimeSpan(0, 0, 0, 0, 25);
             Socket = new AdamSocket();
             Socket.Connect("192.168.1.2", ProtocolType.Tcp, 502);
             modbus = new Modbus(Socket);
@@ -40,6 +51,110 @@ namespace WPFAdam
             inputs.btnPressed += Inputs_btnPressed;
             inputs.redSwitched += Inputs_redSwitched;
             this.Closing += MainWindow_Closing;
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            ToggleLed(1);
+            ToggleLed(2);
+            ToggleLed(3);
+            ToggleLed(4);
+        }
+
+        private void ToggleLed(int led)
+        {
+            statusled[led] = !statusled[led];
+            SetLights();
+        }
+        private void changeStatusOfAllLedsTo(bool v)
+        {
+            int teller = 0;
+            foreach (bool a in statusled)
+            {
+                statusled[teller] = true;
+                teller++;
+            }
+            SetLights();
+        }
+        private void BtnAlarmTest_Click(object sender, RoutedEventArgs e)
+        {
+            changeStatusOfAllLedsTo(true);
+
+            if (dispatcherTimer.IsEnabled)
+            {
+                dispatcherTimer.Stop();
+            }
+            else
+            {
+                dispatcherTimer.Start();
+            }
+        }
+        private void StartStopVentilator()
+        {
+            if (rotateVentilatorTimer.IsEnabled)
+            {
+                rotateVentilatorTimer.Stop();
+                modbus.ForceSingleCoil(17, false);
+            }
+            else
+            {
+                rotateVentilatorTimer.Start();
+                modbus.ForceSingleCoil(17, true);
+            }
+        }
+        private void SetLights()
+        {
+            if (statusled[4])
+            {
+                EllLed4.Fill = new SolidColorBrush(colorLightOn);
+                modbus.ForceSingleCoil(3, true);
+            }
+            else
+            {
+                EllLed4.Fill = new SolidColorBrush(colorLightOff);
+                modbus.ForceSingleCoil(3, false);
+            }
+
+            if (statusled[3])
+            {
+                EllLed3.Fill = new SolidColorBrush(colorLightOn);
+                modbus.ForceSingleCoil(2, true);
+            }
+            else
+            {
+                EllLed3.Fill = new SolidColorBrush(colorLightOff);
+                modbus.ForceSingleCoil(2, false);
+            }
+
+            if (statusled[2])
+            {
+                EllLed2.Fill = new SolidColorBrush(colorLightOn);
+                modbus.ForceSingleCoil(1, true);
+            }
+            else
+            {
+                EllLed2.Fill = new SolidColorBrush(colorLightOff);
+                modbus.ForceSingleCoil(1, false);
+            }
+
+            if (statusled[1])
+            {
+                EllLed1.Fill = new SolidColorBrush(colorLightOn);
+                modbus.ForceSingleCoil(0, true);
+            }
+            else
+            {
+                EllLed1.Fill = new SolidColorBrush(colorLightOff);
+                modbus.ForceSingleCoil(0, false);
+            }
+        }
+
+
+        private void rotateVentilatorTimer_Tick(object sender, EventArgs e)
+        {
+            ventilatorDraaistatus += 5;
+            RotateTransform rotateTransform = new RotateTransform(ventilatorDraaistatus);
+            imgVentilator.RenderTransform = rotateTransform;
         }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
@@ -64,9 +179,7 @@ namespace WPFAdam
             if(nr == 1)
             {
                 Console.WriteLine("Green!!");
-                fan = !fan;
-
-                while(!modbus.ForceSingleCoil(17, fan));
+                StartStopVentilator();
             }
             if(nr == 2)
             {
@@ -77,6 +190,50 @@ namespace WPFAdam
        
         private void btn_Click(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void BtnLed1_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleLed(1);
+        }
+        private void btnLed2_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleLed(2);
+        }
+
+        private void btnLed3_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleLed(3);
+        }
+
+        private void btnLed4_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleLed(4);
+        }
+
+        private void EllLed1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ToggleLed(1);
+        }
+
+        private void EllLed2_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ToggleLed(2);
+        }
+
+        private void EllLed3_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ToggleLed(3);
+        }
+
+        private void EllLed4_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ToggleLed(4);
+        }
+
+        private void btnVentilator_Click(object sender, RoutedEventArgs e)
+        {
+            StartStopVentilator();
         }
     }
 }
---
+++ WPFAdam/email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAdam
+{
+    //class email
+    //{
+    //    private TcpClient client { get; set; } = new TcpClient();
+    //    public email()
+    //    {
+    //        client.Connect("pop.gmail.com", 995, true);
+    //        client.Authenticate("admin@bendytree.com", "YourPasswordHere");
+    //        var count = client.GetMessageCount();
+    //        Message message = client.GetMessage(count);
+    //        Console.WriteLine(message.Headers.Subject);
+    //    }
+
+    //}
+}
